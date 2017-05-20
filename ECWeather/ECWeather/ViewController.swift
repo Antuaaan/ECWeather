3498980600ce20973aa5047e04cb73ac84266ba9
Anton Mansvelt <antonman77@gmail.com>
2017-05-20 15:46:15 +0200

    Cleaning up code for readability

--- ECWeather/ECWeather/ViewController.swift
+++ ECWeather/ECWeather/ViewController.swift
@@ -7,25 +7,17 @@
 //
 
 import UIKit
-import SwiftSky
 import Alamofire
 import SwiftyJSON
 
 let longitude = 18.4241
 let latitude = -33.9249
+let fifteenMinutes = 60*15.0
+let twentyMinutes = 60*20.0
 
 let standard = UserDefaults.standard
-var forecast : Forecast = standard.value(forKeyPath: "forecast") as! Forecast
 
-struct oneDay {
-    var date : String
-    var minTemp : Int
-    var minTime : String
-    var maxTemp : Int
-    var maxTime : String
-}
-
-var allDays : [oneDay] = []
+var allDays : [[String:Any]] = []
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
@@ -33,21 +25,20 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        self.setupSwiftSky()
-//        self.getSwiftSkyForecast()
+        
         if standard.value(forKey: "allDays") != nil{
-            allDays = standard.value(forKey: "allDays") as! [oneDay]
+            allDays = standard.value(forKey: "allDays") as! [[String : Any]]
         }
         
         if allDays.count < 1{
             self.getForecast()
         }
-//        self.tableview.reloadData()
+        
+        Timer.scheduledTimer(timeInterval: twentyMinutes, target: self, selector:#selector(getForecast), userInfo: nil, repeats: true)
     }
     
     func getForecast(){
-        let URL = "http://ec-weather-proxy.appspot.com/forecast/29e4a4ce0ec0068b03fe203fa81d457f/-33.9249,18.4241'"//?delay=5&chaos=0.2"
+        let URL = "http://ec-weather-proxy.appspot.com/forecast/29e4a4ce0ec0068b03fe203fa81d457f/-33.9249,18.4241'?delay=5&chaos=0.2"
         
         let request = Alamofire.request(URL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil)
             .responseJSON { response in
@@ -64,13 +55,12 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
                         let maxTime = Date(timeIntervalSince1970: TimeInterval(one["temperatureMaxTime"].intValue)).ToLocalStringWithFormat("hh:mm")
                         let time = Date(timeIntervalSince1970: TimeInterval(one["time"].intValue)).ToLocalStringWithFormat("yyyy-MM-dd")
                         
-                        let new = oneDay(date: time, minTemp: minTemp, minTime: minTime, maxTemp: maxTemp, maxTime: maxTime)
-//                        print ("oneDay = \(new)")
-                        allDays.append(new)
+                        let oneDic = ["date":time,"minTemp":minTemp,"minTime":minTime,"maxTemp":maxTemp,"maxTime":maxTime] as [String : Any]
+                        allDays.append(oneDic)
                     }
 //                    print ("json = \(json)")
 //                    print ("allDays = \(allDays)")
-//                    standard.set(allDays, forKey: "allDays")
+                    standard.set(allDays, forKey: "allDays")
                     self.tableview.reloadData()
                 }
                 else if (response.result.isFailure){
@@ -87,7 +77,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("\(allDays.count) values expected")
+//        print("\(allDays.count) values expected")
         return allDays.count
     }
     
@@ -95,52 +85,16 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         let cell = self.tableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
         
         let oneValue = allDays[indexPath.row]
-        cell.date.text = oneValue.date
-        cell.time.text = "\(oneValue.minTime) Min : \(oneValue.minTemp) "
-        cell.temp.text = "\(oneValue.maxTime) Max : \(oneValue.maxTemp)"
+        cell.date.text = oneValue["date"] as? String
+        cell.time.text = "\(oneValue["minTime"]!) Min : \(oneValue["minTemp"]!) "
+        cell.temp.text = "\(oneValue["maxTime"]!) Max : \(oneValue["maxTemp"]!)"
         
         return cell
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    /*
-    func setupSwiftSky(){
-        SwiftSky.secret = "29e4a4ce0ec0068b03fe203fa81d457f"
-        SwiftSky.hourAmount = .fortyEight
-        
-        SwiftSky.language = .english
-        
-        SwiftSky.locale = .autoupdatingCurrent
-        
-        SwiftSky.units.temperature = .celsius
-        SwiftSky.units.distance = .kilometer
-        SwiftSky.units.speed = .kilometerPerHour
-        SwiftSky.units.pressure = .millibar
-        SwiftSky.units.precipitation = .millimeter
-        SwiftSky.units.accumulation = .centimeter
-    }
-    
-    func getSwiftSkyForecast(){
-        SwiftSky.get([.current, .minutes, .hours, .days, .alerts],
-                     at: Location(latitude: latitude, longitude: longitude)
-        ) { result in
-            switch result {
-            case .success(let forecast):
-                // do something with forecast
-                print("forecast = \(forecast)")
-                standard.setValue(forecast, forKey: "forecast")
-                
-            case .failure(let error):
-                // do something with error
-                print("failed to get forecast because \(error)")
-            }
-        }
-    }
-*/
 
 }
 extension Date{
